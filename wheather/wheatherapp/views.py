4e5900294a6418d6fcb5fb474bfcb9b14a65549b
vindhyawasan <asingh128ve@gmail.com>
2025-12-28 15:40:51 +0530

    this is a news app site it is work on Django

---
+++ wheather/wheatherapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+import json
+
+def weather(request):
+    if request.method == 'POST':
+        search = request.POST.get('search')  # ✅ fixed case
+
+        url = f"https://newsapi.org/v2/everything?q={search}&from=2025-11-27&sortBy=publishedAt&apiKey=bc3c5afc97c24531bb4d8da1ecd562f5"
+        r = requests.get(url)
+        news = json.loads(r.text)
+
+        if news.get("articles"):
+            articles = news["articles"][0]
+            # print(news)
+            return render(request, 'index.html', {"article": articles})
+
+    return render(request, 'index.html')
